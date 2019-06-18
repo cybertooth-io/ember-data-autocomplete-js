de4335d97dfc7bb0397ae724ce1a42f6e4197d9d
nadnoslen <dan@cybertooth.io>
2019-06-18 08:55:58 -0600

    👷 Removing 2.18 from build process

--- tests/integration/components/autocomplete/component-test.js
+++ tests/integration/components/autocomplete/component-test.js
@@ -6,7 +6,16 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | autocomplete', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('when rendering with handlebars', async function (assert) {
+    await render(hbs`{{autocomplete}}`);
+
+    assert.equal(this.element.getElementsByClassName('algolia-autocomplete').length, 1,
+      'Should be wrapped by autocomplete span');
+    assert.equal(this.element.getElementsByClassName('ember-data-autocomplete-js').length, 2,
+      'Should be two inputs: one for the hint & one for the typed values');
+  });
+
+  test('when rendering with angle-brackets', async function (assert) {
     await render(hbs`<Autocomplete />`);
 
     assert.equal(this.element.getElementsByClassName('algolia-autocomplete').length, 1,
