8892172b792533165598af0ea130e0562f43dff4
nadnoslen <dan@cybertooth.io>
2020-08-26 00:41:14 -0600

    :arrow_up: v3.19.0...v3.20.0

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,30 +1,30 @@
-import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
-import config from './config/environment';
+import AddonDocsRouter, { docsRoute } from "ember-cli-addon-docs/router";
+import config from "dummy/config/environment";
 
 const Router = AddonDocsRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
 });
 
-Router.map(function() {
-  docsRoute(this, function() {
-    this.route('how-it-works');
-    this.route('quickstart');
-    this.route('styling');
-    this.route('demos', function() {
-      this.route('additional-filters');
-      this.route('clearing-chosen');
-      this.route('customized-suggestion');
-      this.route('debug-options');
-      this.route('empty-template');
-      this.route('handlebars-example');
-      this.route('min-length');
-      this.route('no-hint');
-      this.route('page-size');
+Router.map(function () {
+  docsRoute(this, function () {
+    this.route("how-it-works");
+    this.route("quickstart");
+    this.route("styling");
+    this.route("demos", function () {
+      this.route("additional-filters");
+      this.route("clearing-chosen");
+      this.route("customized-suggestion");
+      this.route("debug-options");
+      this.route("empty-template");
+      this.route("handlebars-example");
+      this.route("min-length");
+      this.route("no-hint");
+      this.route("page-size");
     });
   });
 
-  this.route('not-found', { path: '/*path' });
+  this.route("not-found", { path: "/*path" });
 });
 
 export default Router;
